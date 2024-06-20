3604a420568e4b471edc7acfc5b72430c2bb2816
aigle1234 <48964174+aigle1234@users.noreply.github.com>
2024-06-20 20:02:06 +0200

    Update format.ts

--- src/format.ts
+++ src/format.ts
@@ -32,7 +32,7 @@ export function formatLoadCurve(data: { value: string; date: string; interval_le
   );
   return Object.entries(grouped).map(([date, values]) => ({
     date,
-    value: Math.round((1000 * values.reduce((acc, cur) => acc + cur, 0)) / values.length) / 1000,
+    value: Math.round((values.reduce((acc, cur) => acc + cur, 0)) / values.length),
   }));
 }
 
