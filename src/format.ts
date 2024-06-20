eecdbc68cdbadc8fffa372ecbf340dd0553108e9
aigle1234 <48964174+aigle1234@users.noreply.github.com>
2024-06-20 19:16:55 +0200

    Update format.ts

--- src/format.ts
+++ src/format.ts
@@ -5,14 +5,14 @@ export type EnergyDataPoint = { start: string; state: number; sum: number };
 
 export function formatDailyData(data: { value: string; date: string }[]): LinkyDataPoint[] {
   return data.map((r) => ({
-    value: +r.value,
+    value: Math.round((+r.value + Number.EPSILON) * 10) / 10,
     date: dayjs(r.date).format('YYYY-MM-DDTHH:mm:ssZ'),
   }));
 }
 
 export function formatLoadCurve(data: { value: string; date: string; interval_length?: string }[]): LinkyDataPoint[] {
   const formatted = data.map((r) => ({
-    value: +r.value,
+    value: Math.round((+r.value + Number.EPSILON) * 10) / 10,
     date: dayjs(r.date)
       .subtract(parseFloat(r.interval_length?.match(/\d+/)[0] || '1'), 'minute')
       .startOf('hour')
